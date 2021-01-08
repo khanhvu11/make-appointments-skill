8e73ce21cfd5f193e008791d21c14efd7474c272
dv029 <dv029@hdm-stuttgart.de>
2021-01-08 17:01:35 +0100

    test

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 from adapt.intent import IntentBuilder
 from mycroft.util.time import now_local
 import calendar_se as myCal
@@ -8,7 +8,22 @@ class MakeAppointments(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('appointments.make.intent')
+    # @intent_handler('appointments.make.intent')
+    # def handle_appointments_make(self, message):
+    #     now = now_local.date()
+    #     nextAp = myCal.getNextAppointmentDate(now)
+    #     todo = nextAp['Summary']
+    #     dateS = nextAp['Start Date']
+    #     dateE = nextAp['End Date']
+    #     timeS = nextAp['Start Time']
+    #     timeE = nextAp['Start Time']
+    #     if dateS = now:
+    #         self.speak_dialog(
+    #             'Your next appointment is on {} at {} and is entitled {}.'.format(dateS, timeS, todo))
+    #     else:
+    #         self.speak_dialog('You have no Appointment today')
+
+    @intent_handler(IntentBuilder("").require("next.appointment"))
     def handle_appointments_make(self, message):
         now = now_local.date()
         nextAp = myCal.getNextAppointmentDate(now)
@@ -17,11 +32,8 @@ class MakeAppointments(MycroftSkill):
         dateE = nextAp['End Date']
         timeS = nextAp['Start Time']
         timeE = nextAp['Start Time']
-        if dateS = now:
-            self.speak_dialog(
-                'Your next appointment is on {} at {} and is entitled {}.'.format(dateS, timeS, todo))
-        else:
-            self.speak_dialog('You have no Appointment today')
+        self.speak_dialog(
+            'Your next appointment is on {} at {} and is entitled {}.'.format(dateS, timeS, todo))
 
 
 def create_skill():
